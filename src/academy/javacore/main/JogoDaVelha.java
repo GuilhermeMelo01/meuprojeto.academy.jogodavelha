b057774087c843cfb00b1d5664f89cb52b93584e
Guilherme Melo <83541826+GuilhermeMelo01@users.noreply.github.com>
2022-01-07 18:38:34 -0300

    Jogo da velha

---
+++ src/academy/javacore/main/JogoDaVelha.java
@@ -0,0 +1,9 @@
+package academy.javacore.main;
+
+import academy.javacore.game.Jogo;
+
+public class JogoDaVelha {
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo();
+    }
+}
---
+++ src/academy/javacore/player/Computador.java
@@ -0,0 +1,21 @@
+package academy.javacore.player;
+
+import academy.javacore.game.Tabuleiro;
+
+public class Computador extends Jogador{
+
+    public Computador(int jogador){
+        super(jogador);
+        System.out.println("Jogador 'Computador' criado!");
+    }
+
+    @Override
+    public void jogar(Tabuleiro tabuleiro){
+
+    }
+
+    @Override
+    public void Tentativa(Tabuleiro tabuleiro){
+
+    }
+}
